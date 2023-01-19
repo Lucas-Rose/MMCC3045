a57ec19b7482b29776f10355ad028e642b632267
Lucas Rose <lucas58.rose@gmail.com>
2023-01-19 18:50:54 +1100

    Three Working

---
+++ Major/main.js
@@ -0,0 +1,4 @@
+import * as THREE from './node_modules/three/build/three.module.js';
+import {OrbitControls} from 'https://unpkg.com/three@0.144.0/examples/jsm/controls/OrbitControls.js'
+// const controls = new CONTROLS.OrbitControls( camera, renderer.domElement );
+const scene = new THREE.Scene();
